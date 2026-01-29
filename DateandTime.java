1adc86ff7e12f677eee6072198dc0648b6d09bc1
Posa Karthik <posakarthik16@gmail.com>
2026-01-29 11:47:58 +0530

    Added new local date and time program

---
+++ DateandTime.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+public class DateandTime{
+    public static void main(String[] args){
+        LocalDate date = LocalDate.now();
+        System.out.println("Current Date : "+date);
+        LocalTime time = LocalTime.now();
+        System.out.println("Current Time : "+time);
+    }
+}
