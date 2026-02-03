a5580c744eaa067a0b2cc0523328d319126f2a95
Posa Karthik <posakarthik16@gmail.com>
2026-02-03 15:21:32 +0530

    Vector

--- VectorOneshot.java
+++ VectorOneshot.java
@@ -6,22 +6,50 @@ public class VectorOneshot {
     public static void main(String[] args){
 
         //Vector is a Legacy class because it indtroduced in java 1.0 version and it implements List Interface
+        //Duplicates and nulls allowed
+        //follow insertion order and Synchronized(Thread safety)
+        //For iteration have Enumeration legacy cursor
 
 
 
 
         Vector vec = new Vector();
+        //List vec = new Vector();
         vec .add(1);
         vec.add("Jocky Chan");
         vec.add(65000.0);
 
         System.out.println(vec);
 
+        Vector <Integer> v = new Vector<>();
+        //List<Integer> v = new Vector<>();
+        v.add(1);
+
+        //Legacy Cursor
         Enumeration enumr = vec.elements();
 
         while(enumr.hasMoreElements()){
             System.out.println(enumr.nextElement());
         }
+
+        //Methods
+
+        vec.addFirst(3);
+        vec.addLast(1);
+
+        vec.addElement(1);//at first
+
+        vec.addLast(2);//at last
+
+        vec.insertElementAt(32,4);//object and index
+
+        vec.elements();//return Enumeration
+
+        //return first
+        vec.firstElement();
+
+        //returns last
+        vec.lastElement();
 
 
     }
