ff4a0284326638f5c492dc6623e28d3b431a6358
Posa Karthik <posakarthik16@gmail.com>
2026-02-03 12:29:56 +0530

    ArrayList

--- ArrayListOneshot.java
+++ ArrayListOneshot.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.ListIterator;
 public class ArrayListOneshot {
     public static void main(String[] args){
+
+        //ArrayList Is Class it implements List Interface;
         //Default capacity = 10
         //Bettter for read(fast access) and allow duplicates and null objects
         // It follows insertion order and index based Acces(index start from = 0)
