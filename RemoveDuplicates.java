bbeca6c121d9f549a72288e2fa7656bef8acd9c8
Posa Karthik <posakarthik16@gmail.com>
2026-03-10 18:43:38 +0530

    Optimized added

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -1,8 +1,13 @@
+
+import java.util.HashSet;
+
 public class RemoveDuplicates {
     public static void main(String[] args){
 
         int array[] = {1,2,2,4,5,6,1};
 
+
+    //brute force - O(n^2)
         for(int i=0;i<array.length;i++){
             boolean checkDuplicate = false;
             for(int j=0;j<i;j++){
@@ -12,9 +17,24 @@ public class RemoveDuplicates {
                 }
             }
             if(!checkDuplicate){
-                System.out.println(array[i]);
+                System.out.println("BruteForce Array : "+array[i]);
             }
         }
+
+            System.out.println();
+
+    //Optimized - O(n)
+
+            HashSet<Integer> hashSet = new HashSet<>();
+
+            for(int i=0;i<array.length;i++){
+                if(!hashSet.contains(array[i])){
+                    hashSet.add(array[i]);
+                }
+            }
+
+            System.out.println("Optimized Array  :  "+hashSet);
+
     }
-    
+
 }
