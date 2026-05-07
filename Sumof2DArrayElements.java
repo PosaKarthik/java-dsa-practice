f0bfedfc7e95358a0b4a006199038b414e42e6b4
Posa Karthik <posakarthik16@gmail.com>
2026-05-07 13:09:55 +0530

    Sum od 2D array elements added

--- Sumof2DArrayElements.java
+++ Sumof2DArrayElements.java
@@ -3,8 +3,8 @@ public class Sumof2DArrayElements {
 //                       i=0     i=1
         int [][]array={{1,2,3},{4,5,6}};
         //             j=0,1,2 
-        // i=0 -> j=0,1,2 >> 0 0,0 1,0 2
-        //i=1 -> j=0,1,2  >> 1 0,1 1,1 2
+        // i=0 -> j=0,1,2 >> [0][0],[0][1],[0][2]
+        //i=1 -> j=0,1,2  >> [1][0],[1][1],[1][2]
         int sumOfTwoDArray=0;
 
         for(int i=0;i<array.length;i++){
