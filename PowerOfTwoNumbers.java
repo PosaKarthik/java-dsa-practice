3a625e845e88a789404f03ab90d375db3098ca04
Posa Karthik <posakarthik16@gmail.com>
2026-06-05 12:37:08 +0530

    Added another way of solving

--- PowerOfTwoNumbers.java
+++ PowerOfTwoNumbers.java
@@ -5,6 +5,9 @@ public class PowerOfTwoNumbers {
         
         Scanner scanner=new Scanner(System.in);
 
+        System.out.println("==========Way -> 1==========");
+        System.out.println();
+
         System.out.println("Enter base number : ");
         double baseNumber=scanner.nextDouble();
 
@@ -14,6 +17,26 @@ public class PowerOfTwoNumbers {
         int result=(int)Math.pow(baseNumber, exponentNumber);
 
         System.out.println("Result : "+result);
+
+        System.out.println();
+        System.out.println("==========Way -> 2==========");
+        System.out.println();
+
+        System.out.println("Enter base1 number : ");
+        int baseNumber1=scanner.nextInt();
+
+        System.out.println("Enter exponent1 number : ");
+        int exponentNumber1=scanner.nextInt();
+
+
+        int result1=1;
+
+        for(int i=1;i<=exponentNumber1;i++){
+            result1*=baseNumber1;
+        }
+        System.out.println("Result1 : "+result1);
+
+
 
 
 
