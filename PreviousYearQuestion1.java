fe928bd3dea3a2792bc2c0e89ea97e9d1f9f3c80
Posa Karthik <posakarthik16@gmail.com>
2026-06-07 00:17:56 +0530

    Previous year important question1 is solved

--- PreviousYearQuestion1.java
+++ PreviousYearQuestion1.java
@@ -12,7 +12,7 @@ public class PreviousYearQuestion1 {
 
         // ->Withdrawal amount must be a multiple of 100
         // ->Withdrawal amount must be greaterthan 0
-        // ->AccountBalance must be greaterthanor equal to Withdrawal amount
+        // ->AccountBalance must be greaterthan or equal to Withdrawal amount
         // ->After successful Withdrawal,show the remaining balance 
         // ->if any rule fails,print the exact reason
         
@@ -25,6 +25,25 @@ public class PreviousYearQuestion1 {
         // OR 
         // Invalid amount
         // Insufficient balance
+
+
+
+        // Solution:
+
+
+        int withdraw=withdrawAmount;
+        int amount=accountBalance;
+
+        if(withdraw%100!=0 || withdraw<0){
+            System.out.println("Invalid amount");
+        }else if(amount<withdraw){
+            System.out.println("Insufficient Balance");
+        }else{
+            int remainingBalance=amount-withdraw;
+            System.out.println("Withdrawal Successful Remaining balance "+remainingBalance);
+        }
+
+
 
     }
     
