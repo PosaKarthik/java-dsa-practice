27bc70ebb7a2f63d12541cc5009357fca9a9783a
Posa Karthik <posakarthik16@gmail.com>
2026-06-07 00:02:53 +0530

    Previous year question1

---
+++ PreviousYearQuestion1.java
@@ -0,0 +1,31 @@
+import javax.xml.xpath.XPathExpressionException;
+
+public class PreviousYearQuestion1 {
+    public static void main(String[] args) {
+        
+
+        // ATM Withdrawal Validation
+        // -------------------------
+
+        // Arjun goes to an ATM to withdraw money
+        // The ATM follows these rules
+
+        // ->Withdrawal amount must be a multiple of 100
+        // ->Withdrawal amount must be greaterthan 0
+        // ->AccountBalance must be greaterthanor equal to Withdrawal amount
+        // ->After successful Withdrawal,show the remaining balance 
+        // ->if any rule fails,print the exact reason
+        
+        // Input:
+        // withdrawAmount
+        // accountBalance
+
+        // Output:
+        // Withdrawal Successful Remaining balance XXX 
+        // OR 
+        // Invalid amount
+        // Insufficient balance
+
+    }
+    
+}
